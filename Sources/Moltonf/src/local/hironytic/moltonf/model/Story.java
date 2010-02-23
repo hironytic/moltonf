20d161e3f34ed13574b4730d974547a370455f1c
Hiron <hiron@hironytic.com>
2010-02-23 13:42:12 +0000

    Moltonf の現時点のソースコード（ほとんど作れてない）を追加した。

---
+++ Sources/Moltonf/src/local/hironytic/moltonf/model/Story.java
@@ -0,0 +1,62 @@
+/*
+ * Moltonf
+ *
+ * Copyright (c) 2010 Project Moltonf
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package local.hironytic.moltonf.model;
+
+import java.util.List;
+
+/**
+ * ストーリーを表すインタフェースです。
+ * 通常、ストーリーは1回のプレイデータ全体に相当します。
+ */
+public interface Story {
+
+	/**
+	 * このストーリーに含まれる StoryPeriod を返します。
+	 * @return StoryPeriod のリスト
+	 */
+	public List<StoryPeriod> getPeriods();
+	
+	//public ??? getAvatarList(); // TODO:
+	
+	/**
+	 * ストーリー中で扱う相対 URI のベース URI を返します。
+	 * @return ベース URI 文字列
+	 */
+	public String getBaseUri();
+	
+	/**
+	 * 村のフルネームを返します。
+	 * @return 村のフルネーム
+	 */
+	public String getVillageFullName();
+	
+	/**
+	 * 村の状態を返します。
+	 * @return 村の状態
+	 */
+	public VillageState getVillageState();
+	
+}
---
+++ Sources/Moltonf/src/local/hironytic/moltonf/model/VillageState.java
@@ -0,0 +1,43 @@
+/*
+ * Moltonf
+ *
+ * Copyright (c) 2010 Project Moltonf
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package local.hironytic.moltonf.model;
+
+/**
+ * 村の状態を表す列挙型
+ */
+public enum VillageState {
+	/** プロローグ中 */
+	PROLOGUE,
+	
+	/** 進行中 */
+	PROGRESS,
+	
+	/** エピローグ中 */
+	EPILOGUE,
+	
+	/** ゲーム終了 */
+	GAMEOVER,
+}
