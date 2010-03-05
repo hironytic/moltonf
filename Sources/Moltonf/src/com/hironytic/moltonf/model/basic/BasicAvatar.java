732daecce8f990aeef23065658a9bc9583acc9c3
Hiron <hiron@hironytic.com>
2010-03-05 15:12:58 +0000

    モデルインタフェースの基本実装を追加中。

---
+++ Sources/Moltonf/src/com/hironytic/moltonf/model/basic/BasicAvatar.java
@@ -0,0 +1,131 @@
+/*
+ * Moltonf
+ *
+ * Copyright (c) 2010 Project Moltonf
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.hironytic.moltonf.model.basic;
+
+import java.net.URI;
+
+import com.hironytic.moltonf.model.Avatar;
+
+/**
+ * 基本的な Avatar 実装
+ */
+public class BasicAvatar implements Avatar {
+
+    /** 省略名 */
+    private String abbreviatedName;
+ 
+    /** アバターの ID */
+    private String avatarId;
+    
+    /** 顔アイコン画像への URI */
+    private URI faceIconUri;
+    
+    /** フルネーム */
+    private String fullName;
+    
+    /** 短い名前 */
+    private String shortName;
+    
+    /**
+     * @see com.hironytic.moltonf.model.Avatar#getAbbreviatedName()
+     */
+    @Override
+    public String getAbbreviatedName() {
+        return abbreviatedName;
+    }
+
+    /**
+     * @see com.hironytic.moltonf.model.Avatar#getAvatarId()
+     */
+    @Override
+    public String getAvatarId() {
+        return avatarId;
+    }
+
+    /**
+     * @see com.hironytic.moltonf.model.Avatar#getFaceIconUri()
+     */
+    @Override
+    public URI getFaceIconUri() {
+        return faceIconUri;
+    }
+
+    /**
+     * @see com.hironytic.moltonf.model.Avatar#getFullName()
+     */
+    @Override
+    public String getFullName() {
+        return fullName;
+    }
+
+    /**
+     * @see com.hironytic.moltonf.model.Avatar#getShortName()
+     */
+    @Override
+    public String getShortName() {
+        return shortName;
+    }
+
+    /**
+     * abbreviatedName をセットします。
+     * @param abbreviatedName セットしたい abbreviatedName の値
+     */
+    public void setAbbreviatedName(String abbreviatedName) {
+        this.abbreviatedName = abbreviatedName;
+    }
+
+    /**
+     * avatarId をセットします。
+     * @param avatarId セットしたい avatarId の値
+     */
+    public void setAvatarId(String avatarId) {
+        this.avatarId = avatarId;
+    }
+
+    /**
+     * faceIconUri をセットします。
+     * @param faceIconUri セットしたい faceIconUri の値
+     */
+    public void setFaceIconUri(URI faceIconUri) {
+        this.faceIconUri = faceIconUri;
+    }
+
+    /**
+     * fullName をセットします。
+     * @param fullName セットしたい fullName の値
+     */
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    /**
+     * shortName をセットします。
+     * @param shortName セットしたい shortName の値
+     */
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+}
---
+++ Sources/Moltonf/src/com/hironytic/moltonf/model/basic/BasicStory.java
@@ -0,0 +1,141 @@
+/*
+ * Moltonf
+ *
+ * Copyright (c) 2010 Project Moltonf
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.hironytic.moltonf.model.basic;
+
+import java.net.URI;
+import java.util.List;
+
+import com.hironytic.moltonf.model.Avatar;
+import com.hironytic.moltonf.model.Story;
+import com.hironytic.moltonf.model.StoryPeriod;
+import com.hironytic.moltonf.model.VillageState;
+
+/**
+ * Story を実装した基本的なクラス。
+ */
+public class BasicStory implements Story {
+
+    /** 登場人物のリスト */
+    private List<Avatar> avatarList;
+    
+    /** ベース URI */
+    private URI baseUri;
+    
+    /** この Story に含まれる StoryPeriod のリスト */
+    private List<StoryPeriod> periods;
+    
+    /** 村の完全名 */
+    private String villageFullName;
+    
+    /** 村の状態 */
+    private VillageState villageState;
+    
+    /**
+     * コンストラクタ
+     */
+    public BasicStory() {
+    }
+    
+    /**
+     * @see com.hironytic.moltonf.model.Story#getAvatarList()
+     */
+    @Override
+    public List<Avatar> getAvatarList() {
+        return avatarList;
+    }
+
+    /**
+     * @see com.hironytic.moltonf.model.Story#getBaseUri()
+     */
+    @Override
+    public URI getBaseUri() {
+        return baseUri;
+    }
+
+    /**
+     * @see com.hironytic.moltonf.model.Story#getPeriods()
+     */
+    @Override
+    public List<StoryPeriod> getPeriods() {
+        return periods;
+    }
+
+    /**
+     * @see com.hironytic.moltonf.model.Story#getVillageFullName()
+     */
+    @Override
+    public String getVillageFullName() {
+        return villageFullName;
+    }
+
+    /**
+     * @see com.hironytic.moltonf.model.Story#getVillageState()
+     */
+    @Override
+    public VillageState getVillageState() {
+        return villageState;
+    }
+
+    /**
+     * avatarList をセットします。
+     * @param avatarList セットしたい avatarList の値
+     */
+    public void setAvatarList(List<Avatar> avatarList) {
+        this.avatarList = avatarList;
+    }
+
+    /**
+     * baseUri をセットします。
+     * @param baseUri セットしたい baseUri の値
+     */
+    public void setBaseUri(URI baseUri) {
+        this.baseUri = baseUri;
+    }
+
+    /**
+     * periods をセットします。
+     * @param periods セットしたい periods の値
+     */
+    public void setPeriods(List<StoryPeriod> periods) {
+        this.periods = periods;
+    }
+
+    /**
+     * villageFullName をセットします。
+     * @param villageFullName セットしたい villageFullName の値
+     */
+    public void setVillageFullName(String villageFullName) {
+        this.villageFullName = villageFullName;
+    }
+
+    /**
+     * villageState をセットします。
+     * @param villageState セットしたい villageState の値
+     */
+    public void setVillageState(VillageState villageState) {
+        this.villageState = villageState;
+    }
+}
