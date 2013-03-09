c2d8182e61e0ab31075a84f7fabadf1ea589e825
hiron <hiron@hironytic.com>
2013-03-09 14:40:08 +0900

    refs #6 ワークスペース一覧でワークスペースを削除できるようにした。

--- Sources/MoltonfDroid/src/com/hironytic/moltonfdroid/WorkspaceListActivity.java
+++ Sources/MoltonfDroid/src/com/hironytic/moltonfdroid/WorkspaceListActivity.java
@@ -29,15 +29,23 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.database.Cursor;
-import android.os.Build;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.hironytic.moltonfdroid.model.Workspace;
 import com.hironytic.moltonfdroid.model.WorkspaceManager;
@@ -95,7 +103,7 @@ public class WorkspaceListActivity extends ListActivity {
     
     /** ワークスペース管理オブジェクト */
     private WorkspaceManager workspaceManager;
-    
+
     /**
      * @see android.app.Activity#onCreate(android.os.Bundle)
      */
@@ -103,10 +111,12 @@ public class WorkspaceListActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.workspace_list);
-        
+
         // ワークスペースの一覧を更新
         workspaceManager = new WorkspaceManager(getApplicationContext());
         reloadList();
+        
+        registerForContextMenu(getListView());
     }
 
     /**
@@ -121,7 +131,7 @@ public class WorkspaceListActivity extends ListActivity {
         
         super.onDestroy();
     }
-
+    
     /**
      * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
      */
@@ -154,6 +164,48 @@ public class WorkspaceListActivity extends ListActivity {
         return result;
     }
 
+    /**
+     * @see android.app.Activity#onCreateContextMenu(android.view.ContextMenu, android.view.View, android.view.ContextMenu.ContextMenuInfo)
+     */
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+        if (v == getListView()) {
+            AdapterContextMenuInfo listMenuInfo = (AdapterContextMenuInfo)menuInfo;
+            WorkspaceListItem listItem = (WorkspaceListItem)getListAdapter().getItem(listMenuInfo.position);
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.workspace_item_option, menu);
+            menu.setHeaderTitle(listItem.getTitle());
+        } else {        
+            super.onCreateContextMenu(menu, v, menuInfo);
+        }
+    }
+
+    /**
+     * @see android.app.Activity#onContextItemSelected(android.view.MenuItem)
+     */
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        boolean result = false;
+        int itemId = item.getItemId();
+        switch (itemId) {
+        case R.id.menu_workspace_remove_data:
+            result = processMenuRemoveData(item);
+            break;
+        default:
+            result = super.onContextItemSelected(item);
+            break;
+        }
+        return result;
+    }
+
+    /**
+     * @see android.app.ListActivity#onListItemClick(android.widget.ListView, android.view.View, int, long)
+     */
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+    }
+    
     /**
      * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
      */
@@ -219,5 +271,36 @@ public class WorkspaceListActivity extends ListActivity {
         workspaceManager.save(ws);
         
         reloadList();
+    }
+
+    /**
+     * 観戦データの削除メニューが選ばれたときの処理
+     * @param item メニュー
+     * @return 処理したらtrue
+     */
+    private boolean processMenuRemoveData(MenuItem item) {
+        AdapterContextMenuInfo listMenuInfo = (AdapterContextMenuInfo)item.getMenuInfo();
+        int position = listMenuInfo.position;
+        final WorkspaceListItem listItem = (WorkspaceListItem)getListAdapter().getItem(position);
+
+        // 削除していいですか？
+        String message = getResources().getString(R.string.message_workspace_remove_data_alert, listItem.getTitle());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message);
+        builder.setNegativeButton(R.string.no, new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // 何もしない
+            }
+        });
+        builder.setPositiveButton(R.string.yes, new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                WorkspaceListActivity.this.workspaceManager.delete(listItem.getWorkspaceId());
+                reloadList();
+            }
+        });
+        builder.show();
+        return true;
     }
 }
